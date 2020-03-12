a454a70680c004d50598e70e1ac75e9518c9d944
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-12 20:58:25 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -8,6 +8,12 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
-def get_the_keys(groceries)
-  #code your solution here!
+def get_the_values(groceries)
+ groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+ groceries.keys
 end
